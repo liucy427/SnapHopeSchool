e2bfc0295bf74a07e8f8a3985fdd04c4a88c13ad
cccczl001 <cccczl@hotmail.com>
2020-09-07 19:36:03 +0800

    %rel 到 %dst

--- locale/lang-zh_CN.js
+++ locale/lang-zh_CN.js
@@ -799,7 +799,7 @@ SnapTranslator.dict.zh_CN = {
     'key %key pressed?':
         '按下了 %key 键？',
     '%rel to %dst':
-        '角色到 %dst 的 %rel',    
+        '%rel 到 %dst',    
     '%asp at %loc' :
         '%asp 于 %loc',    
     'sprites':
